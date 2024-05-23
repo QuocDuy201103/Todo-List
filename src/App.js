c76cd9c8b027806c918088527898bca1ec549569
QuocDuy201103 <duyyduy123@gmail.com>
2024-05-23 16:54:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [work, setwork] = useState('')
+  console.log(work);
   return (
-    <div className="App">
-      app
+    <div className="flex gap-8 h-screen justify-center items-center">
+      <input
+        className=" outline-none border border-blue-500 px-4 py-2 w-[400px]"
+        value={work}
+        onChange={e => setwork(e.target.value)}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white"
+      >
+        Add
+      </button>
     </div>
   );
 }
